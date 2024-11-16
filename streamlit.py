fbf661965eab92627f6f4f490828e4d1802f364d
andreimedved1123@gmail.com <amedved1@sheffield.ac.uk>
2024-11-16 13:43:04 +0000

    Initial Commit

--- streamlit.py
+++ streamlit.py
@@ -2,7 +2,10 @@ import streamlit as st
 
 def run_streamlit():
     st.title("Brain Rot Translator")
-    
+    a = st.sidebar.date_input('Start date', value=None, min_value=None, max_value=None, key=None)
+    text_input = st.text_input('Enter some text')
+    if text_input is not None:
+        st.text(f"Text input:\n{text_input}")
     st.write("Upload your input file here (only .txt files).")
     uploaded_file = st.file_uploader("Choose a file", type="txt")
 
@@ -16,7 +19,7 @@ def run_streamlit():
         st.text_area("File Contents", input_text, height=200, disabled=True)
 
         if st.button("Translate"):
-         
+            
             translated_text = brain_rot_translate(input_text) 
             st.text_area("Translated Output", translated_text, height=200)
 
