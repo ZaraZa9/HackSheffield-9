5224ac036e29a3cb4b0c159e9d98fa9fd534bbcf
andreimedved1123@gmail.com <amedved1@sheffield.ac.uk>
2024-11-17 11:41:06 +0000

    Some final debugging

--- streamlit.py
+++ streamlit.py
@@ -117,6 +117,8 @@ def brain_rot_translate(input_text, selected_decade, decade_data=None):
     return response.text, rot_frequency
 
 def generate_image_with_rate_limit(img_prompt):
+    st.write(openAI_client)
+    st.write(img_prompt)
     img_response = openAI_client.images.generate(
         model="dall-e-3",
         prompt="A surreal image of " + img_prompt,
@@ -191,9 +193,9 @@ def run_streamlit():
     if input_text and st.button("Translate"):
         if generate_images_checkbox:
             with st.spinner("Generating pre-translation image..."):
-                st.write(input_text)
-                st.write(selected_decade)
-                st.write("LOOK HERE")
+                #st.write(input_text)
+                #st.write(selected_decade)
+                #st.write("LOOK HERE")
                 pre_translation_image_url = generate_image_with_rate_limit(input_text)
                 if pre_translation_image_url:
                     st.image(pre_translation_image_url, caption="Image Before Translation", use_column_width=True)
